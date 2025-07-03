ff990275b6152db0f0338b087f33552e4ab1fd61
CalvinCheng0306 <calvinc0306@gmail.com>
2025-07-03 13:49:22 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/home.html');
+});
+
+app.get('/home', (req,res) => {
+  res.sendFile(__dirname + '/home.html');
+});
+
+app.get('/home2', (req,res) => {
+  res.sendFile(__dirname + '/home2.html');
+});
+
+app.get('/contact', (req,res) => {
+  res.sendFile(__dirname + '/contact.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at https://localhost:${port}`);
+});
